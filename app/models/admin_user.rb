afd06b3729a2493c20135e4c13badbfd47b2def5
Axel Yoshimoto <axelyoshimoto@Axels-MacBook-Pro.local>
2012-12-24 00:03:41 +0100

    define some more validations for admin user

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,9 +1,15 @@
 class AdminUser < ActiveRecord::Base
   include Bitfields
+
   bitfield :role_bits,
     1 => :wizard,
     2 => :runner,
     4 => :trainee
+
+
+  EMAIL_PATTERN = /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,5})$/i
+  USER_PATTERN = /^[A-Za-z0-9\-_@]{4,40}$/i
+
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
@@ -11,12 +17,17 @@ class AdminUser < ActiveRecord::Base
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
+
   attr_accessible :email, :password, :password_confirmation, :remember_me, :role_bits, :username
-  # attr_accessible :title, :body
+
 
   validates :password, :confirmation => true
   validates_presence_of :password, :on => :create
+  validates :email, :uniqueness => true,
+                    :format => { :with => EMAIL_PATTERN }
+  validates :username, :format => { :with => USER_PATTERN },
+                       :uniqueness => true
+
 
   before_validation do |user|
     if user.password.blank? && user.password_confirmation.blank?
