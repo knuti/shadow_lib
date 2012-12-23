d9ffef24aec8fed7610b79f556ede57efff70e3f
Axel Yoshimoto <axelyoshimoto@Axels-MacBook-Pro.local>
2012-12-23 21:31:19 +0100

    let admin user be updatable without providing pw

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,10 +2,20 @@ class AdminUser < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, 
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+  validates :password, :confirmation => true
+  validates_presence_of :password, :on => :create
+
+  before_validation do |user|
+    if user.password.blank? && user.password_confirmation.blank?
+      user.password = user.password_confirmation = nil
+    end
+  end
+
 end
