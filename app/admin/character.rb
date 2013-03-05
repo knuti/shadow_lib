d7ee159e4a9da5f854a7f05768b1a85f2181a5e5
Axel Yoshimoto <axelyoshimoto@Axels-MacBook-Pro.local>
2013-03-05 22:40:16 +0100

    restructuring Character class and editing

--- app/admin/character.rb
+++ app/admin/character.rb
@@ -2,14 +2,17 @@ ActiveAdmin.register Character do
 
   menu label: "Characters", parent: "Shadowrun Wiki"
 
+  filter :name
+  filter :char_class
+  filter :race
+
   index do
     column :name
-    column I18n.t('character.race'), :race
-    column I18n.t('character.char_class'), :char_class
-    column I18n.t('character.admin_user') do |char|
+    column :race
+    column :char_class
+    column :admin_user do |char|
       char.admin_user.username
     end
-    column :created_at
     column :updated_at
     column :visibility
     default_actions if can? :manage, Character
@@ -19,41 +22,7 @@ ActiveAdmin.register Character do
     render 'listing'
   end
 
-  filter :name
-  filter :char_class
-  filter :race
-
-  form do |f|
-    f.inputs "Character Details" do
-      f.input :name
-      f.input :race, collection: I18n.t('character.races')
-      f.input :char_class, collection: I18n.t('character.classes')
-      f.input :age
-      f.input :armor_items
-      f.input :ballistic_armor
-      f.input :burst_armor
-      f.input :charisma
-      f.input :connections
-      f.input :constitution
-      f.input :cyberware
-      f.input :description
-      f.input :essence
-      f.input :gear
-      f.input :initiative
-      f.input :intelligence
-      f.input :mana
-      f.input :other_stuff
-      f.input :reaction
-      f.input :sex, collection: I18n.t('character.gender')
-      f.input :skills
-      f.input :speed
-      f.input :spells
-      f.input :strength
-      f.input :admin_user_id, as: :hidden, value: character.admin_user_id || current_admin_user.id
-      f.input :visibility, collection: I18n.t('character.visibility')
-      f.input :weapons
-      f.input :willpower
-    end
-    f.actions
+  form do
+    render 'form'
   end
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -3,26 +3,20 @@ class Character < ActiveRecord::Base
 
   belongs_to :admin_user
 
-  CHAR_ATTRIBUTES = {
-    :constitution => 'KONSTITUTION',
-    :speed => 'SCHNELLIGKEIT',
-    :strength => 'STAERKE',
-    :charisma => 'CHARISMA',
-    :intelligence => 'INTELLIGENZ',
-    :willpower => 'WILLENSKRAFT',
-    :essence => 'ESSENZ',
-    :mana => 'MAGIE'
-  }
+  CHAR_CLASSES = %w(ki-adept sorcerer shaman decker rigger aspect-wizard)
+  CHAR_RACES = %w(orc dwarf elf human troll)
+  CHAR_VISIBILITIES = %w(public runner creator)
+  CHAR_ATTRIBUTES = %w(constitution speed strength charisma intelligence willpower essence mana)
 
-  validates :char_class, :inclusion => { :in => I18n.t('character.classes') }
-  validates :race, :inclusion => { :in => I18n.t('character.races')}
-  validates :visibility, :inclusion => { :in => I18n.t('character.visibility') }
+  validates :char_class, :inclusion => { :in => CHAR_CLASSES }
+  validates :race, :inclusion => { :in => CHAR_RACES }
+  validates :visibility, :inclusion => { :in => CHAR_VISIBILITIES }
   # validates :attribute, :format => { :with => /[0-9]+\(\d\)/ }, :allow_nil => true
-  validates_numericality_of :mana, :ballistic_armor, :burst_armor, :greater_than => 0, :allow_nil => true, :only_integer => true
+  validates_numericality_of :mana, :ballistic_armor, :burst_armor, greater_than: 0, allow_nil: true, only_integer: true
 
-  validates_presence_of :admin_user_id, :on => :create
+  validates_presence_of :admin_user_id, on: :create
 
-  scope :public_available, where(:visibility => I18n.t('character.visibility.public'))
-  scope :runner_allowed, where(:visibility => I18n.t('character.visibility'))
+  scope :public_available, where(visibility: 'public')
+  scope :runner_allowed, where(visibility: 'runner')
 
 end
