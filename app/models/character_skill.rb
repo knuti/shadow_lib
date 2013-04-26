351729c4a27876062e166835f2601db0bf527bea
Axel Yoshimoto <axelyoshimoto@Axels-MacBook-Pro.local>
2013-04-26 10:42:23 +0200

    enable CharacterSkill to be saved with additional information

--- app/models/character_skill.rb
+++ app/models/character_skill.rb
@@ -1,5 +1,6 @@
 class CharacterSkill < ActiveRecord::Base
 
+  attr_accessible :character_id, :skill_id, :rank, :specialization, :specialization_rank
   # set_table_name :characters_skills
 
   belongs_to :skill
