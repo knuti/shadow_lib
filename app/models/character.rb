3a63048bee5f11b7af538f4c16b79c9b293a3c87
Axel Yoshimoto <axelyoshimoto@Axels-MacBook-Pro.local>
2013-04-26 10:43:06 +0200

    out of edit form, build skills for character

--- app/models/character.rb
+++ app/models/character.rb
@@ -8,6 +8,8 @@ class Character < ActiveRecord::Base
   has_many :skills, through: :character_skills
   has_and_belongs_to_many :spells
 
+  before_save :build_skills
+
   CHAR_CLASSES = %w(ki-adept decker rigger sorcerer shaman aspect-wizard)
   CHAR_RACES = %w(orc dwarf elf human troll)
   CHAR_VISIBILITIES = %w(public runner creator)
@@ -24,4 +26,21 @@ class Character < ActiveRecord::Base
   scope :public_available, where(visibility: 'public')
   scope :runner_allowed, where(visibility: 'runner')
 
+  protected
+
+  def build_skills
+    self.temp_skills.split("\r\n").each do|char_skill|
+      tmp = char_skill.gsub(/(\[|\]|\(|\))/, "").split
+      if skill = Skill.find_by_name(tmp[0])
+        if cs = CharacterSkill.where(skill_id: skill.id, character_id: self.id).first
+          cs.update_attributes! rank: tmp[1], specialization: tmp[2], specialization_rank: tmp[3]
+        else
+          CharacterSkill.create! character_id: self.id, skill_id: skill.id, rank: tmp[1], specialization: tmp[2], specialization_rank: tmp[3]
+        end
+      else
+        next
+      end
+    end
+  end
 end
+
