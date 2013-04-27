e715edf7c1339e5ab920664a6c3b881d8df6491d
Axel Yoshimoto <axelyoshimoto@Axels-MacBook-Pro.local>
2013-04-27 21:01:58 +0200

    forgot to make ref_atr accessible

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,6 +1,6 @@
 class Skill < ActiveRecord::Base
 
-  attr_accessible :name, :description
+  attr_accessible :name, :description, :reference_attribute
 
   has_many :character_skills
   has_many :characters, through: :character_skills
