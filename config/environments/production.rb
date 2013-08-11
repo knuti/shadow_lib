c66079af067e1b49fe3f329b8bb2ab6a1524e306
Axel Yoshimoto <axel.yoshimoto@gmail.com>
2013-08-11 13:20:50 +0200

    add configuration option for mail sending

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,4 +67,13 @@ ShadowLib::Application.configure do
 
   # Configuration for active admin/devise mailings
   config.action_mailer.default_url_options = { :host => 'yoshimoto.me' }
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'yoshimoto.me',
+    user_name: ENV['SMTP_USERNAME'],
+    password: ENV['SMTP_PASSWORD'],
+    authentication: :login,
+    enable_starttls_auto: true
+  }
 end
